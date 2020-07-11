046764829a01a839bc3fdf33677db56deab86920
mikusaa <mikusaaaaa@gmail.com>
2020-07-11 18:52:12 +0800

    Update my site

--- conf.py
+++ conf.py
@@ -78,8 +78,7 @@ head_addon = r'''
 <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
 '''
 
-footer_addon = 
-'''
+footer_addon = '''
 <link rel="apple-icon" sizes="180x180" href="/apple-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
